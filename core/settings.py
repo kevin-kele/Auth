822725ba5202841694eb4a68f218152f7c15323d
kevin-kele <kevinkele1@gmail.com>
2021-12-16 13:24:25 +0100

    django ini

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v+p6ggr$to+r71((qa(k&7nahwcol_mq$ubu_0x9h871j#rlrg'
+SECRET_KEY = 'django-insecure-c+_y-7$f6xsjdg7!x=bs4i!a*gyo3%e*1f(qmr528*x$e(5tnb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
 ]
 
 MIDDLEWARE = [
